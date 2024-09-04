8b1dd0bbdb46be1575b0a89455c914ca93e3b27a
chr867 <124451478+chr867@users.noreply.github.com>
2024-09-04 17:46:32 +0900

    [Silver IV] Title: 카드2, Time: 312 ms, Memory: 50936 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,7 +1,8 @@
 from collections import deque
+import sys
 deque = deque()
 
-for i in range(int(input())):
+for i in range(int(sys.stdin.readline())):
     deque.append(i+1)
 
 while len(deque) > 1:
